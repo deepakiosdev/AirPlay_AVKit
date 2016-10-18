94ec186ff9d8f665cc8476c95c7d94abada920ff
Dipak <dipak@PFTBLR-IMAC-004.local>
2016-10-18 17:09:36 +0530

    Created sample project for AirplayDemo using AVKit.

--- AirPlay_AVKit/AppDelegate.m
+++ AirPlay_AVKit/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +17,10 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+    // Set AudioSession
+      //NSError *sessionError = nil;
+     //[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:&sessionError];
     return YES;
 }
 
